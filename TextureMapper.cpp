d5c5ce70569db6ee6b8fb01a068d92db5af37016
Leo Winiecki <leodwiniecki@outlook.com>
2019-02-27 09:32:01 -0800

    asdf

--- TextureMapper.cpp
+++ TextureMapper.cpp
@@ -17,6 +17,26 @@ void TextureMapper::align() {
 }
 
 void TextureMapper::patchSearch(cv::Mat source, cv::Mat target) {
+	for (int t = 0; t < source.frames; t++) {
+		// INITIALIZATION - uniform random assignment
+        for (int y = 0; y < source.size().height; y++) {
+            for (int x = 0; x < source.size().width; x++) {
+                int dx = randomInt(patchSize, target.width-patchSize-1);
+                int dy = randomInt(patchSize, target.height-patchSize-1);
+                int dt = randomInt(0, target.frames-1);
+                out(x, y, t, 0) = dx;
+                out(x, y, t, 1) = dy;
+                out(x, y, t, 2) = dt;
+                out(x, y, t, 3) = distance(source, target, mask,
+                                           x, y, t,
+                                           dx, dy, dt,
+                                           patchSize, HUGE_VAL);
+            }
+        }
+	}
+	
+	bool forwardSearch = true;
+	cv::Mat dx = out.channel(0), dy = out.channel(1), dt = out.channel(2), error = out.channel(3);
 	
 }
 
