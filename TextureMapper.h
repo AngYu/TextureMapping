26eb762f54f41f0d694dd599d89b60e02eabecf3
Leo Winiecki <leodwiniecki@outlook.com>
2019-02-28 21:23:20 -0800

    fixed error in header

--- TextureMapper.h
+++ TextureMapper.h
@@ -16,10 +16,10 @@ private:
     void reconstruct();
     int Mixi();
     cv::Mat patchSearch(std::vector<cv::Mat> source, std::vector<cv::Mat> target, int iterations, int patchSize);
-    void vote(cv::Mat patchSearchResult);
+    void vote(cv::Mat completenessPatchMatches, cv::Mat coherencePatchMatches);
     std::vector<std::vector<int[3]>> findSourcePatches(cv::Mat completenessPatchMatches, cv::Mat coherencePatchMatches, int x, int y, int t);
     bool isInTargetPatch(cv::Vec<float, 4> targetMatch, int x, int y, int t);
-    int Tixi(std::vector<int[3]> patches);
+    int Tixi(std::vector<int[3]> completenessPatches, std::vector<int[3]> coherencePatches);
     float distance(int sx, int sy, int st,
                     int tx, int ty, int tt,
                     int patchSize, float threshold);
